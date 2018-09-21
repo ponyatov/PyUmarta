bf6209f6578d11f8fdf9d8c8b34b1fc4b09acd24
Dmitry Ponyatov <dponyatov@gmail.com>
2018-09-21 18:24:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -1,4 +1,35 @@
 # piumarta object VM: see doc/
 
-from ply.lex import lex
+import ply.lex  as lex
+import ply.yacc as yacc
+
+class MObject:
+	pass
+
+import os,sys
+
+SRC = open(sys.argv[1]).read()
+print SRC
+
+tokens = ['SYM']
+
+def t_SYM(t):
+	r'[a-zA-Z0-9_]+'
+	return t
+
+def t_error(t): raise SyntaxError(t)
+
+lexer = lex.lex()
+
+def p_REPL_none(p):
+	r'REPL : '
+	print p
+
+def p_REPL(p):
+	r'REPL : REPL SYM'
+	print p
+
+def p_error(p): raise SyntaxError(p)
+
+parser = yacc.yacc(debug=None,write_tables=False)
 
