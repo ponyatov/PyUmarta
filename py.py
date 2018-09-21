eadceb390a3c5ead805a3eb53f1d1d8b94e17fa7
Dmitry Ponyatov <dponyatov@gmail.com>
2018-09-21 18:49:23 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -3,21 +3,46 @@
 import ply.lex  as lex
 import ply.yacc as yacc
 
-class MObject:
-	def __init__(self,V): self.val = V
+pool = []
+
+class Object:
+	def __init__(self,V):
+		self.tag = self.__class__.__name__ ; self.val = V
+		self.attr = {} ; self.nest = []
+		global pool ; pool += [self]
 	def __repr__(self): return self.dump()
 	def dump(self,depth=0): return self.head()
-	def head(self,prefix=''): return '%s<%s>'%(prefix,self.val)
+	def head(self,prefix=''): return '%s<%s:%s>'%(prefix,self.tag,self.val)
+
+class Stack(Object): pass
+
+S = Stack('DATA')
 
 import os,sys
 
 SRC = open(sys.argv[1]).read()
 print SRC
 
-tokens = ['SYM']
+tokens = ['SYM','SAVE','QQ','Q','DOT']
 
 t_ignore = ' \t\r\n'
 t_ignore_COMMENT = r'\#.*'
+
+def t_QQ(t):
+	r'\?\?'
+	return t
+
+def t_Q(t):
+	r'\?'
+	return t
+
+def t_SAVE(t):
+	r'\.save'
+	return t
+
+def t_DOT(t):
+	r'\.'
+	return t
 
 def t_SYM(t):
 	r'[a-zA-Z0-9_]+'
@@ -29,10 +54,26 @@ lexer = lex.lex()
 
 def p_REPL_none(p):
 	r'REPL : '
-
 def p_REPL(p):
 	r'REPL : REPL SYM'
-	print MObject(p[2])
+	Object(p[2])
+
+def p_SAVE(p):
+	r'REPL : REPL SAVE SYM'
+	import pickle
+	pickle.dump(pool,open(p[3],'w'))
+
+def p_QQ(p):
+	r'REPL : REPL QQ'
+	print S ; print pool
+
+def p_Q(p):
+	r'REPL : REPL Q'
+	print S
+
+def p_DOT(p):
+	r'REPL : REPL DOT'
+	S.flush()
 
 def p_error(p): raise SyntaxError(p)
 
